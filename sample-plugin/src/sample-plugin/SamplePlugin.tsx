0ffaa5f37f60309e9378aec674ba8c82abca9aa1
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2026-05-18 16:56:09 +0900

    fix: HAWNG-1995 Update HawtIO version to 4.4.0.redhat-00018

--- sample-plugin/src/sample-plugin/SamplePlugin.tsx
+++ sample-plugin/src/sample-plugin/SamplePlugin.tsx
@@ -1,11 +1,11 @@
-import { PageSection, PageSectionVariants, Text, TextContent } from '@patternfly/react-core'
+import { Content, PageSection } from '@patternfly/react-core'
 import React from 'react'
 
 export const SamplePlugin: React.FunctionComponent = () => (
-  <PageSection variant={PageSectionVariants.light}>
-    <TextContent>
-      <Text component='h1'>Sample Plugin</Text>
-      <Text component='p'>This is a sample Hawtio plugin that is discovered dynamically at runtime.</Text>
-    </TextContent>
+  <PageSection hasBodyWrapper={false}>
+    <Content>
+      <Content component='h1'>Sample Plugin</Content>
+      <Content component='p'>This is a sample Hawtio plugin that is discovered dynamically at runtime.</Content>
+    </Content>
   </PageSection>
 )
